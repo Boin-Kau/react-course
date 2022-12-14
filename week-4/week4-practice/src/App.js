6cc92151be053c642f4865ef405f5aa71e06a931
Boin Kim <charles1236@kau.kr>
2022-12-14 22:33:35 +0900

    :sparkles: feat: 4주차 프로젝트 추가

---
+++ week-4/week4-practice/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import styled from "styled-components";
+import { cartNumState } from "./recoil/cart";
+
+function App() {
+
+  return (
+    <ParentWrapper>
+      <Child1 />
+      <Child2/>
+    </ParentWrapper>
+  );
+}
+
+export default App;
+
+const Child1 = () => {
+
+  const [num, setNum] = useRecoilState(cartNumState);
+
+  const [name, setName] = useState('');
+
+  return (
+    <Wrapper>
+      <h1>Child1</h1>
+      <div>
+        <button onClick={() => setNum(num - 1)}>-</button>
+        <span>{num}</span>
+        <button onClick={() => setNum(num + 1)}>+</button>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Child2 = () => {
+
+  const num = useRecoilValue(cartNumState);
+  const setNum = useSetRecoilState(cartNumState);
+
+  return (
+    <Wrapper>
+      <h1>Child2</h1>
+      <div>
+        <span>{num}</span>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  background-color: white;
+`
+
+const ParentWrapper = styled(Wrapper)`
+  background-color: yellow;
+  padding: 16px
+
+`;
---
+++ week-4/week4-practice/src/recoil/cart.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const cartNumState = atom({
+  key: "cartNumState",
+  default: 0,
+});
